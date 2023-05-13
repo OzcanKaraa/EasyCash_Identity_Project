f6596b1059a47880d403c6b3be133d82e5c52806
Özcan Kara <ozcankara810@gmail.com>
2023-05-13 23:41:15 +0300

    some changes

--- EasyCash_Identity_Project.EntityLayer/Concrete/CustomerAccount.cs
+++ EasyCash_Identity_Project.EntityLayer/Concrete/CustomerAccount.cs
@@ -1,22 +1,21 @@
 ﻿using System;
-using System.Collection.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EasyCash_Identity_Project.EntityLayer.Concrete
 {
-		public Class CustomerAccount
-	{
-		public int CustomerAccountID { get; set; }
+    internal class CustomerAccount
+    {
+        public int CustomerAccountID { get; set; } // Müşteri Hesabı ID
 
-		public string  CustomerAccountNumber { get; set; }
+        public string CustomerAccountNumber { get; set; } // Müşteri Hesap Numarası
 
-		public string CustomerAccountCurrency { get; set; } /*Müşteri Para türü*/
+        public string CustomerAccountCurrency { get; set; } // Müşteri Para türü
 
-		public string CustomerAccountBalance { get; set; } /*Müşteri Toplam Bakiye*/
+        public string CustomerAccountBalance { get; set; } // Müşteri Toplam Bakiye
 
-		public string CustomerAccountBankBranch { get; set; } /*Banka Subesi*/
-
-}
+        public string CustomerAccountBankBranch { get; set; } // Banka Subesi
+    }
 }
--- EasyCash_Identity_Project.EntityLayer/Concrete/CustomerAccountProcess.cs
+++ EasyCash_Identity_Project.EntityLayer/Concrete/CustomerAccountProcess.cs
@@ -1,20 +1,18 @@
 ﻿using System;
-using System.Collection.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EasyCash_Identity_Project.EntityLayer.Concrete
 {
-    public Class AccountProcess
+    internal class CustomerAccountProcess
     {
-
-            public int CustomerAccountProcessID { get; set; }
-             public string ProcessType { get; set; } // Islem türü
-             public decimal AMount { get; set; } // Miktar
-            public DateTime ProcessDate { get; set; } //Islem Tarihi 
-            //Gonderici ve Alici 
-
-
-}
+        public int CustomerAccountProcessID { get; set; }  // Müşteri Hesabı ID
+        public string ProcessType { get; set; } // Islem türü
+        public decimal AMount { get; set; } // Miktar
+        public DateTime ProcessDate { get; set; } // Islem Tarihi 
+           
+        //Gonderici ve Alici 
+    }
 }
