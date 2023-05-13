13741fc72fe0528ca9b3c85d366a7e2fea50b03b
Özcan Kara <ozcankara810@gmail.com>
2023-05-14 00:24:46 +0300

    some  changes

--- EasyCash_Identity_Project.DataAccesLayer/Concrete/Context.cs
+++ EasyCash_Identity_Project.DataAccesLayer/Concrete/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using EasyCash_Identity_Project.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -14,8 +15,12 @@ namespace EasyCash_Identity_Project.DataAccessLayer.Concrete
         {
             //Baglanti adresi
             optionsBuilder.UseSqlServer("server=DESKTOP-U82UVSC;initial catalog=EasyCashDB;integrated Security=true;"); 
-
-
         }
+        public DbSet<CustomerAccount> CustomerAccounts{ get; set; } // <CustomerAccount> C# Class ismi
+                                                                    // customerAccounts Sql de Tablo ismi
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; } //<CustomerAccountProcess> C# Class ismi ,
+                                                                                    //  CustomerAccountProcesses Sql de Tablo ismi
+
+
     }
 }
